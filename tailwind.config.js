5f1ae1885466a1d80075b92efefbce389bd6ea0f
Scott Sumner <scottsumner@Scotts-Air.hsd1.fl.comcast.net>
2025-03-20 13:49:49 -0400

    upload all files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,12 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/globals.css', // ensure your CSS file is scanned
   ],
   safelist: [
-    'py-6',
+    {
+      pattern: /^py-/, // safelist any py-* classes
+    },
   ],
   darkMode: 'class',
   theme: {
